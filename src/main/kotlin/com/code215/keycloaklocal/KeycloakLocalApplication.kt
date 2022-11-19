3a441b4241fe4a7f158b7b0dc3df3a4cbc52ca69
Michael Wangia <michael.w@getslatewell.com>
2022-11-20 00:58:34 +0300

    first commit

---
+++ src/main/kotlin/com/code215/keycloaklocal/KeycloakLocalApplication.kt
@@ -0,0 +1,11 @@
+package com.code215.keycloaklocal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KeycloakLocalApplication
+
+fun main(args: Array<String>) {
+    runApplication<KeycloakLocalApplication>(*args)
+}
---
+++ src/test/kotlin/com/code215/keycloaklocal/KeycloakLocalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.code215.keycloaklocal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KeycloakLocalApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
